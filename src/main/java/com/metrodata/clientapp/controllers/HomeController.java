c014ff594b934414d4a50a1d70ff3cb8b635e2e9
Maulana Rafael Irianto <maulanarafael36@gmail.com>
2023-05-09 14:37:51 +0700

    Fragment and Thymeleaf Executive

--- src/main/java/com/metrodata/clientapp/controllers/HomeController.java
+++ src/main/java/com/metrodata/clientapp/controllers/HomeController.java
@@ -3,12 +3,24 @@ package com.metrodata.clientapp.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping
 public class HomeController {
     
-    @GetMapping("/home")
-    public String index(Model model) {
-        return "index";
+    @GetMapping("/homeemployee")
+    public String indexEmployee(Model model) {
+        return "employee/index";
+    }
+
+    @GetMapping("/homeexecutive")
+    public String indexExecutive(Model model) {
+        return "executive/index";
+    }
+
+    @GetMapping("/homehr")
+    public String indexHr(Model model) {
+        return "hr/index";
     }
 }
